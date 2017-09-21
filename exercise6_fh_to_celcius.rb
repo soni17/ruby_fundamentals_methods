ba74e41d8fd3ad7140e324704ad0acb17938392f
soni17 <elb004@gmail.com>
2017-09-21 17:49:36 -0400

    make sure entry is a number and if not ask user to enter number

--- exercise6_fh_to_celcius.rb
+++ exercise6_fh_to_celcius.rb
@@ -1,2 +1,7 @@
-puts "enter a temperature in farenheit: "
-entry = gets.chomp.to_i
+puts "Enter a temperature in farenheit: "
+entry = gets.to_i
+
+while (entry==0)
+  puts "Value entered is not a number. Please enter a number:"
+  entry = gets.to_i
+end
